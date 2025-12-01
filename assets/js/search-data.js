eff0c18017923358a32bd653b430c8517b88c860
ryancyeung <ryancyeung@users.noreply.github.com>
2025-12-01 11:51:58 +0000

    Deploying to gh-pages from @ ryancyeung/ryancyeung.github.io@ce4457c6f84eb521bfe9ea0a60b779176f41d30d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,27 +58,7 @@ ninja.data = [{
           section: "News",},{id: "news-finally-launched-the-big-website-reformat-still-under-construction-warning-construction",
           title: 'Finally launched the big website reformat. Still under construction! :warning: :construction:',
           description: "",
-          section: "News",},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
-            },},{id: "projects-computational-methods",
+          section: "News",},{id: "projects-computational-methods",
           title: 'computational methods',
           description: "open-source AI/ML/NLP tools",
           section: "Projects",handler: () => {
